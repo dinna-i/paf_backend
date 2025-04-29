70cf482fe6e9148772946fba2a03703dfd0a3093
Nadeen-Gunathilake <nadeen.guna05@gmail.com>
2025-04-30 00:18:20 +0530

    comment DTO

---
+++ src/main/java/com/sapp/social/dto/CommentResponse.java
@@ -0,0 +1,22 @@
+package com.sapp.social.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentResponse {
+    private Long commentId;
+    private String content;
+    private LocalDateTime createdAt;
+    private Long userId;
+    private String userName;
+    private Long postId;
+    private Long parentCommentId;
+    private List<CommentResponse> replies;
+}
